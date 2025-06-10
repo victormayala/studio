fa89be32b5a47e3efc742c84d1cd7e00d398ba02
Victor Ayala <vicmanayaari@gmail.com>
2025-06-10 02:16:14 +0000

    Feel free to make the left sidebar wider

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -22,8 +22,8 @@ import {
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
-const SIDEBAR_WIDTH = "16rem"
-const SIDEBAR_WIDTH_MOBILE = "18rem"
+const SIDEBAR_WIDTH = "20rem" // Increased from 16rem
+const SIDEBAR_WIDTH_MOBILE = "22rem" // Increased from 18rem
 const SIDEBAR_WIDTH_ICON = "3rem"
 const SIDEBAR_KEYBOARD_SHORTCUT = "b"
 
@@ -758,4 +758,3 @@ export {
   SidebarTrigger,
   useSidebar,
 }
-
