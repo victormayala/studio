e97a471f0eef1981be6d9d5dc4a54ba9690b2e4a
Victor Ayala <vicmanayaari@gmail.com>
2025-06-10 01:32:55 +0000

    why is not working? nothing is changing

--- src/components/customizer/RightPanel.tsx
+++ src/components/customizer/RightPanel.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import AiAssistant from './AiAssistant';
@@ -6,7 +7,7 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 
 export default function RightPanel() {
   return (
-    <Card className="w-full md:w-80 lg:w-96 h-full flex-shrink-0 shadow-lg rounded-lg flex flex-col">
+    <Card className="w-72 md:w-80 lg:w-96 h-full flex-shrink-0 shadow-lg rounded-lg flex flex-col">
       <CardHeader className="border-b">
         <CardTitle className="font-headline text-lg text-foreground">AI Design Assistant</CardTitle>
       </CardHeader>
