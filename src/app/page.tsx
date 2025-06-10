1206761ce73e911cf5e9e852b3dd85120afe62bc
Victor Ayala <vicmanayaari@gmail.com>
2025-06-10 01:16:07 +0000

    move the right sidebar to the right and expand the middle section full w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,13 @@ export default function CustomizerPage() {
         
         <SidebarInset className="flex flex-col flex-1 overflow-hidden">
           <AppHeader />
-          <main className="flex flex-1 gap-4 md:gap-6 overflow-y-auto"> {/* scrollable main content, removed p-4 md:p-6 */}
-            <DesignCanvas />
-            <RightPanel />
-          </main>
+          {/* New flex container for DesignCanvas and RightPanel */}
+          <div className="flex flex-1 gap-4 md:gap-6 overflow-hidden p-4 md:p-6">
+            <main className="flex-1 overflow-y-auto"> {/* DesignCanvas container, takes most space */}
+              <DesignCanvas />
+            </main>
+            <RightPanel /> {/* RightPanel sits to the right of the main content */}
+          </div>
         </SidebarInset>
       </div>
     </SidebarProvider>
