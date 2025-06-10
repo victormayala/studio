ea2cc2aab79228fef936798902f8f459feafc845
Victor Ayala <vicmanayaari@gmail.com>
2025-06-10 01:10:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning={true}>
         {children}
         <Toaster />
       </body>
